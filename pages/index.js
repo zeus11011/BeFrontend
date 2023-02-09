ea67e3b674fd6bdf3a61052d770879d938a489a7
zeus11011 <naikanish13@gmail.com>
2023-02-09 13:04:06 +0530

    second command

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
-<<<<<<< HEAD
 import Drawer from "../Components/Drawer";
-=======
->>>>>>> main
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div className={styles.main}>
       <Drawer />
       <div className={styles.cont}>
         <h1>cont</h1>
       </div>
-=======
-    <div className={styles.container}>
-      <h1></h1>
->>>>>>> main
     </div>
   );
 }
