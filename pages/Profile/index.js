1366f7a18ad4dbe0e68392182c0837c3e6db7e89
Dhananjay Pawar <111731004+Dhananjay2601@users.noreply.github.com>
2023-02-11 16:13:05 +0530

    profile page done

--- pages/Profile/index.js
+++ pages/Profile/index.js
@@ -20,13 +20,111 @@ const index = () => {
           </div>
         </div>
         <div className={styles.container3}>
-          <div className={[styles.div2, styles.border].join(" ")}>
+          <div className={[styles.div1, styles.border].join(" ")}>
             <h1 className={styles.h1}>Personal</h1>
-            <h2>Update your photo and other details</h2>
+            <div className={styles.personal1}>
+              <div className={styles.name}>
+                <div>
+                  <h2 className={styles.h2}>First Name</h2>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="fname"
+                    placeholder="Name"
+                    className={styles.textfield_1}
+                  ></input>
+                </div>
+              </div>
+              <div className={styles.name2}>
+                <div>
+                  <h2 className={styles.h2}>Last Name</h2>
+                </div>
+                <div>
+                  <input
+                    type="name"
+                    name="lname"
+                    placeholder="Surname"
+                    className={styles.textfield_1}
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className={styles.personal2}>
+              <div className={styles.detail}>
+                <div>
+                  <h2 className={styles.h2}>Email</h2>
+                </div>
+                <div>
+                  <input
+                    type="fname"
+                    name="fname"
+                    placeholder="Email"
+                    className={styles.textfield_2}
+                  ></input>
+                </div>
+              </div>
+              <div className={styles.detail}>
+                <div>
+                  {" "}
+                  <h2 className={styles.h2}>Password</h2>
+                </div>
+                <div>
+                  {" "}
+                  <input
+                    type="lname"
+                    name="lname"
+                    placeholder="Password"
+                    className={styles.textfield_2}
+                  ></input>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className={styles.div2}>
-            <h1 className={styles.h1}>Acedemic</h1>
-            <h2>Update your photo and other details</h2>
+          <div className={styles.container4}>
+            <div className={styles.div2}>
+              <h1 className={styles.h1}>Academic</h1>
+              <div className={styles.academic1}>
+                <div className={styles.year}>
+                  <div>
+                    <h2 className={styles.h2}>Year</h2>
+                  </div>
+                  <div>
+                    <input
+                      type="number"
+                      name="yearS"
+                      placeholder="Start"
+                      className={styles.textfield_3}
+                    ></input>
+                  </div>
+                </div>
+                <div className={styles.year}>
+                  <div>
+                    <input
+                      type="number"
+                      name="yearE"
+                      placeholder="End"
+                      className={styles.textfield_4}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.academic2}>
+                <div className={styles.desig}>
+                  <div>
+                    <h2 className={styles.h2}>Designation</h2>
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      name="fname"
+                      placeholder="Designation"
+                      className={styles.textfield_5}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
