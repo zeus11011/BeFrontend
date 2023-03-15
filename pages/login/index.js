8164fcf6b9f015467cea36c682afa7e4de7e2f9c
Dhananjay Pawar <111731004+Dhananjay2601@users.noreply.github.com>
2023-03-15 14:42:30 +0530

    calendar updated

--- pages/login/index.js
+++ pages/login/index.js
@@ -3,67 +3,65 @@ import styles from "../../styles/Login.module.scss";
 import Image from "next/image";
 import { Checkbox } from "@mui/material";
 import Link from "next/link";
-function login() {
+
+function admin() {
   return (
     <div className={styles.main}>
-      <div className={styles.container}>
-        <Image src={"/gec.svg"} alt="" height={330} width={400} />
+      <div className={styles.con1}>
+        <Image src={"/gec.svg"} alt="" height={300} width={300} />
       </div>
-      <div className={styles.container2}>
-        <div className={styles.maincont}>
-          <div className={styles.h1container}>
-            <h1 className={styles.h1}> LOGIN</h1>
-            <h2 className={styles.h2C}>STUDENT | ADMIN</h2>
-          </div>
-          <div className={styles.link2}>
-            <Link href="/" style={{ color: "black" }}>
-              {" "}
-              {"Click here for admin login"}
-            </Link>
-          </div>
-          <div className={styles.box}>
-            <div className={styles.box2}>
-              <h2 className={styles.h2}>Username</h2>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                className={styles.textfiled}
-              ></input>
+      <div className={styles.con2}>
+        <div className={styles.card}>
+          <div className={styles.maincont}>
+            <div className={styles.h1container}>
+              <h1 className={styles.h1}> LOGIN</h1>
+              <h2 className={styles.h2C}>STAFF | FACULTY</h2>
             </div>
-          </div>
 
-          <div className={styles.box}>
-            <div className={styles.box2}>
-              <h2 className={styles.h2}>Password</h2>
-              <input
-                type="password"
-                name="email"
-                placeholder="Password"
-                className={styles.textfiled}
-              ></input>
+            <div className={styles.box}>
+              <div className={styles.box2}>
+                <h2 className={styles.h2}>Username</h2>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className={styles.textfiled}
+                ></input>
+              </div>
             </div>
-          </div>
-          <div className={styles.box5}>
-            <Checkbox
-              label="checkbox"
-              value="true"
-              onChange=""
-              className={styles.checkbox}
-            ></Checkbox>
-            Remember Password?
-          </div>
-          <div className={styles.box4}>
-            <button name="login" onClick="" className={styles.button}>
-              Log In
-            </button>
-            <div className={styles.box3}>
-              {"Forgot "}
-              <div className={styles.link}>
-                <Link href="/" style={{ color: "black" }}>
-                  {" "}
-                  {"Username / Password?"}
-                </Link>
+
+            <div className={styles.box}>
+              <div className={styles.box2}>
+                <h2 className={styles.h2}>Password</h2>
+                <input
+                  type="password"
+                  name="email"
+                  placeholder="Password"
+                  className={styles.textfiled}
+                ></input>
+              </div>
+            </div>
+            <div className={styles.box5}>
+              <Checkbox
+                label="checkbox"
+                value="true"
+                onChange=""
+                className={styles.checkbox}
+              ></Checkbox>
+              Remember Password?
+            </div>
+            <div className={styles.box4}>
+              <div className={styles.buttbox}>
+                <button name="login" onClick="" className={styles.button}>
+                  Log In
+                </button>
+              </div>
+
+              <div className={styles.box3}>
+                {"Forgot "}
+                <div className={styles.link}>
+                  <Link href="/"> {"Username / Password?"}</Link>
+                </div>
               </div>
             </div>
           </div>
@@ -72,9 +70,8 @@ function login() {
     </div>
   );
 }
+export default admin;
 
-export default login;
-
-login.getLayout = function PageLayout(page) {
+admin.getLayout = function PageLayout(page) {
   return <> {page}</>;
 };
