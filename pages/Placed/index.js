c4bfe30813111ef4445a12666d529d5be1e192b5
zeus11011 <naikanish13@gmail.com>
2023-03-21 13:17:01 +0530

    miuix data grid added

--- pages/Placed/index.js
+++ pages/Placed/index.js
@@ -1,323 +1,323 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Select from "react-select";
 import styles from "../../styles/Placed.module.scss";
 import TableScrollbar from "react-table-scrollbar";
+import { DataGrid } from "@mui/x-data-grid";
+
 const data = [
   {
+    id: 1,
     cgpa: "8.5",
     company: "TCS",
     name: "John Doe",
     roll: "101",
-    department: "Computer Science",
+    department: "CS",
     package: "6 LPA",
     email: "johndoe@email.com",
   },
   {
+    id: 2,
     cgpa: "8.5",
     company: "TCS",
     name: "Jane Doe",
     roll: "102",
-    department: "Mechanical Engineering",
+    department: "MEC",
     package: "5 LPA",
     email: "janedoe@email.com",
   },
 
   {
+    id: 3,
     cgpa: "8.5",
     company: "TCS",
     name: "Jim Smith",
     roll: "103",
-    department: "Electronics Engineering",
+    department: "EE",
     package: "7 LPA",
     email: "jimsmith@email.com",
   },
   {
+    id: 4,
     cgpa: "8.5",
     company: "TCS",
     name: "Sarah Johnson",
     roll: "104",
-    department: "Civil Engineering",
+    department: "Civil",
     package: "6.5 LPA",
     email: "sarahjohnson@email.com",
   },
   {
+    id: 5,
     cgpa: "8.5",
     company: "TCS",
     name: "Bob Wilson",
     roll: "105",
-    department: "Computer Science",
+    department: "CS",
     package: "5.5 LPA",
     email: "bobwilson@email.com",
   },
   {
+    id: 6,
     cgpa: "8.5",
     company: "TCS",
     name: "Emily Davis",
     roll: "106",
-    department: "Mechanical Engineering",
+    department: "MEC",
     package: "6 LPA",
     email: "emilydavis@email.com",
   },
   {
+    id: 7,
     cgpa: "8.5",
     company: "TCS",
     name: "Michael Brown",
     roll: "107",
-    department: "Electronics Engineering",
+    department: "EE",
     package: "7.5 LPA",
     email: "michaelbrown@email.com",
   },
   {
+    id: 8,
     cgpa: "8.5",
     company: "TCS",
     name: "John Doe",
     roll: "101",
-    department: "Computer Science",
+    department: "CS",
     package: "6 LPA",
     email: "johndoe@email.com",
   },
 
   {
+    id: 9,
     cgpa: "8.5",
     company: "TCS",
     name: "Jane Doe",
     roll: "102",
-    department: "Mechanical Engineering",
+    department: "MEC",
     package: "5 LPA",
     email: "janedoe@email.com",
   },
 
   {
+    id: 10,
     cgpa: "8.5",
     company: "TCS",
     name: "Jim Smith",
     roll: "103",
-    department: "Electronics Engineering",
+    department: "EE",
     package: "7 LPA",
     email: "jimsmith@email.com",
   },
   {
+    id: 11,
     cgpa: "8.5",
     company: "TCS",
     name: "Sarah Johnson",
     roll: "104",
-    department: "Civil Engineering",
+    department: "Civil",
     package: "6.5 LPA",
     email: "sarahjohnson@email.com",
   },
   {
+    id: 12,
     cgpa: "8.5",
     company: "TCS",
     name: "Bob Wilson",
     roll: "105",
-    department: "Computer Science",
+    department: "CS",
     package: "5.5 LPA",
     email: "bobwilson@email.com",
   },
   {
+    id: 13,
     cgpa: "8.5",
     company: "TCS",
     name: "Emily Davis",
     roll: "106",
-    department: "IT Engineering",
+    department: "IT",
     package: "6 LPA",
     email: "emilydavis@email.com",
   },
   {
+    id: 14,
     cgpa: "8.5",
     company: "TCS",
     name: "Michael Brown",
     roll: "107",
-    department: "IT Engineering",
+    department: "IT",
     package: "7.5 LPA",
     email: "michaelbrown@email.com",
   },
   {
+    id: 15,
     cgpa: "8.5",
     company: "TCS",
     name: "Jennifer Clark",
     roll: "108",
-    department: "Civil Engineering",
+    department: "Civil",
     package: "6 LPA",
     email: "jenniferclark@email.com",
   },
   {
+    id: 16,
     cgpa: "8.5",
     company: "TCS",
     name: "William Jones",
     roll: "109",
-    department: "Computer Science",
+    department: "CS",
     package: "5.5 LPA",
     email: "williamjones@email.com",
   },
   {
+    id: 17,
     cgpa: "8.5",
     company: "TCS",
     name: "Amanda Smith",
     roll: "110",
-    department: "Mechanical Engineering",
+    department: "MEC",
     package: "6.5 LPA",
     email: "amandasmith@email.com",
   },
   {
+    id: 18,
     cgpa: "8.5",
     company: "TCS",
     name: "David Johnson",
     roll: "111",
-    department: "Electronics Engineering",
+    department: "EE",
     package: "7 LPA",
     email: "davidjohnson@email.com",
   },
   {
+    id: 19,
     cgpa: "8.5",
     company: "TCS",
     name: "James Davis",
     roll: "112",
-    department: "Civil Engineering",
+    department: "Civil",
     package: "6 LPA",
     email: "jamesdavis@email.com",
   },
   {
+    id: 20,
     cgpa: "8.5",
     company: "TCS",
     name: "Daniel Wilson",
     roll: "113",
-    department: "Computer Science",
+    department: "CS",
     package: "5.5 LPA",
     email: "danielwilson@email.com",
   },
   {
+    id: 21,
     cgpa: "8.5",
     company: "TCS",
     name: "Emily Jones",
     roll: "114",
-    department: "Mechanical Engineering",
+    department: "MEC",
     package: "6.5 LPA",
     email: "emilyjones@email.com",
   },
   {
+    id: 22,
     cgpa: "8.5",
     company: "TCS",
     name: "Michael Smith",
     roll: "115",
-    department: "Electronics Engineering",
+    department: "EE",
     package: "7 LPA",
     email: "michaelsmith@email.com",
   },
   {
+    id: 23,
     cgpa: "8.5",
     company: "TCS",
     name: "Jennifer Davis",
     roll: "116",
-    department: "Civil Engineering",
+    department: "Civil",
     package: "6 LPA",
     email: "jenniferdavis@email.com",
   },
   {
+    id: 24,
     cgpa: "8.5",
     company: "TCS",
     name: "William Clark",
     roll: "117",
-    department: "Computer Science",
+    department: "CS",
     package: "5.5 LPA",
     email: "williamclark@email.com",
   },
 
   {
+    id: 25,
     cgpa: "8.5",
     company: "TCS",
     name: "John Doe",
     roll: "101",
-    department: "Computer Science",
+    department: "CS",
     package: "6 LPA",
     email: "johndoe@email.com",
   },
   {
+    id: 26,
     cgpa: "8.5",
     company: "TCS",
     name: "Jane Doe",
     roll: "102",
-    department: "Mechanical Engineering",
+    department: "MEC",
     package: "5 LPA",
     email: "janedoe@email.com",
   },
   {
+    id: 27,
     cgpa: "8.5",
     company: "TCS",
     name: "Jim Smith",
     roll: "103",
-    department: "Electronics Engineering",
+    department: "EE",
     package: "7 LPA",
     email: "jimsmith@email.com",
   },
   {
+    id: 28,
     cgpa: "8.5",
     company: "TCS",
     name: "Sarah Johnson",
     roll: "104",
-    department: "Civil Engineering",
+    department: "Civil",
     package: "6.5 LPA",
     email: "sarahjohnson@email.com",
   },
   // Add more dummy student data as needed
 ];
+const columns = [
+  { field: "roll", headerName: "Roll no.", width: 80 },
+  { field: "name", headerName: "Name", width: 150 },
+  { field: "email", headerName: "Email", width: 200 },
+  { field: "cgpa", headerName: "CGPA", width: 80 },
+  { field: "company", headerName: "Company", width: 150 },
+  { field: "department", headerName: "Department", width: 180 },
+  { field: "package", headerName: "Package", width: 100 },
+];
 
 const Placed = () => {
   const [students, setStudents] = useState(data);
+  const [selected, setSelected] = useState(undefined);
 
-  const sortByName = () => {
-    const sortedStudents = [...students].sort((a, b) => {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
+  useEffect(() => {
+    const d = data.map((element, index) => {
+      return { ...element, id: index };
     });
-    setStudents(sortedStudents);
-  };
-
-  const sortByDepartment = () => {
-    const sortedStudents = [...students].sort((a, b) => {
-      if (a.department < b.department) return -1;
-      if (a.department > b.department) return 1;
-      return 0;
-    });
-    setStudents(sortedStudents);
-  };
-
-  const sortByPackage = () => {
-    const sortedStudents = [...students].sort((a, b) => {
-      if (a.package < b.package) return -1;
-      if (a.package > b.package) return 1;
-      return 0;
-    });
-    setStudents(sortedStudents);
-  };
-
-  const filterByDepartment = (e) => {
-    const filteredStudents = data.filter(
-      (student) => student.department === e.target.value
-    );
-    setStudents(filteredStudents);
-  };
-  const optionsf = [
-    { value: "Computer Science", label: "Computer Science" },
-    { value: "Mechanical Engineering", label: "Mechanical " },
-    { value: "Electronics Engineering", label: "Electronics " },
-    { value: "Civil Engineering", label: "Civil" },
-    { value: "IT Engineering", label: "IT" },
-    { value: "Electrical Engineering", label: "Electrical" },
-  ];
-  const optionss = [
-    { value: "Name", label: "By Name" },
-    { value: "Department", label: "By Department" },
-    { value: "Package", label: "By Package" },
-  ];
-
+    console.log(d);
+    setStudents(d);
+  }, []);
   return (
     <div className={styles.main}>
       <div className={styles.main1}>
         <div className={styles.container2}>
-          <Select
+          {/* <Select
             onChange={filterByDepartment}
             className={styles.dropdown}
             placeholder={"Filter"}
@@ -328,10 +328,10 @@ const Placed = () => {
             placeholder={"Sort"}
             options={optionss}
             onChange={sortByName}
-          ></Select>
+          ></Select> */}
         </div>
         <div className={styles.container3}>
-          <TableScrollbar>
+          {/* <TableScrollbar>
             <table className={styles.table} scro>
               <thead className={styles.thead}>
                 <tr className={styles.tr}>
@@ -358,41 +358,58 @@ const Placed = () => {
                 ))}
               </tbody>
             </table>
-          </TableScrollbar>
+          </TableScrollbar> */}
+          <DataGrid
+            rows={students}
+            columns={columns}
+            sx={{ fontSize: 16, fontWeight: "500" }}
+            hideFooter
+            isRowSelectable={(params) => {
+              setSelected(params.row);
+            }}
+            pageSizeOptions={[10, 20, 30]}
+
+            // se
+          />
         </div>
       </div>
       <div className={styles.main2}>
-        <div className={styles.dp}>
-          <Image
-            alt=""
-            src={"/dp.jpg"}
-            width={150}
-            height={150}
-            className={styles.img}
-            style={{
-              borderRadius: "50%",
-            }}
-          />
-        </div>
-        <h1 className={styles.h1}>John Doe</h1>
-        <p className={styles.p}>91+ 6969696969</p>
-        <p className={styles.p}>roma@gmail.com</p>
-        <div className={styles.card}>
-          <p className={styles.p1}>Company</p>
-          <p className={styles.p2}>TCS</p>
-        </div>
-        <div className={styles.card}>
-          <p className={styles.p1}>Department</p>
-          <p className={styles.p2}>CS</p>
-        </div>
-        <div className={styles.card}>
-          <p className={styles.p1}>CGPA</p>
-          <p className={styles.p2}>6.5</p>
-        </div>
-        <div className={styles.card}>
-          <p className={styles.p1}>Package</p>
-          <p className={styles.p2}>7 LPA</p>
-        </div>
+        {selected != undefined ? (
+          <>
+            <div className={styles.dp}>
+              <Image
+                alt=""
+                src={"/dp.jpg"}
+                width={150}
+                height={150}
+                className={styles.img}
+                style={{
+                  borderRadius: "50%",
+                }}
+              />
+            </div>
+            <h1 className={styles.h1}>{selected.name}</h1>
+            <p className={styles.p}>{selected.email}</p>
+            <div className={styles.card}>
+              <p className={styles.p1}>Company</p>
+              <p className={styles.p2}>{selected.company}</p>
+            </div>
+            <div className={styles.card}>
+              <p className={styles.p1}>Department</p>
+              <p className={styles.p2}>{selected.department}</p>
+            </div>
+            <div className={styles.card}>
+              <p className={styles.p1}>CGPA</p>
+              <p className={styles.p2}>{selected.cgpa}</p>
+            </div>
+            <div className={styles.card}>
+              <p className={styles.p1}>Package</p>
+              <p className={styles.p2}>{selected.package}</p>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
