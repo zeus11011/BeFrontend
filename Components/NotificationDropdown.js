60b7448c9e4c0baed6ac7e252838fb8185001aa4
hk2257853 <hk2257853@gmail.com>
2023-06-13 19:15:47 +0530

    notification dropdown css, profile fix

--- Components/NotificationDropdown.js
+++ Components/NotificationDropdown.js
@@ -4,7 +4,7 @@ import "rsuite/dist/rsuite.min.css";
 import { useSelector } from "react-redux";
 import PhoneIcon from "@rsuite/icons/Phone";
 import SearchPeopleIcon from "@rsuite/icons/SearchPeople";
-import "../styles/ProfileDropdown.less";
+import styles from "../styles/NotificationDropdown.module.scss";
 import { Icon } from "@iconify/react";
 
 const NotificationDropdown = ({icon}) => {
@@ -12,8 +12,8 @@ const NotificationDropdown = ({icon}) => {
   return (
       <div>
       <Dropdown title={<Icon icon={icon} height={"4rem"} />}>    
-        <Dropdown.Item icon={<SearchPeopleIcon />}>test 1</Dropdown.Item>
-        <Dropdown.Item icon={<PhoneIcon />}>test 2</Dropdown.Item>
+        <Dropdown.Item className={styles.dropdownitem} icon={<SearchPeopleIcon />}>test 1</Dropdown.Item>
+        <Dropdown.Item className={styles.dropdownitem} icon={<PhoneIcon />}>test 2</Dropdown.Item>
       </Dropdown>
     </div>
   );
